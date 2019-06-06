eac78801426b0910b67390d9f4dcd538c1e654c2
RsSabbirAhmed <rssabbirahmed1996@gmail.com>
2019-06-07 04:37:05 +0600

    Add hight and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,11 @@ const forecast = (latitude, longitude, callback) => {
         body.daily.data[0].summary +
           " It is currently " +
           body.currently.temperature +
-          " degrees out. There is a " +
+          " degrees out. This is hight tody is " +
+          body.daily.data[0].temperatureHigh +
+          " with a low of " +
+          body.daily.data[0].temperatureLow +
+          ". There is a " +
           body.currently.precipProbability +
           "% chance of rain."
       );
